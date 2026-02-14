01e5edf44fa931c56507de31251fb7ea3e9c0e41
jpnski <68269686+jpnski@users.noreply.github.com>
2026-02-13 22:59:05 -0800

    Update remap.lua

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -21,3 +21,5 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
+
+vim.keymap.set("t", "<C-q>", '<C-\\><C-n>', {silent = true})
