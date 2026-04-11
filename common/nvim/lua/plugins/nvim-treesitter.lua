bc2fdd4b6ce66b0b7da4a87e7765439d52ea30b3
j <jpnski@protonmail.ch>
2026-04-11 01:23:38 -0700

    fix markdown preview instal

--- common/nvim/lua/plugins/nvim-treesitter.lua
+++ common/nvim/lua/plugins/nvim-treesitter.lua
@@ -5,12 +5,12 @@ return {
         lazy = false,
         build = ':TSUpdate',
         config = function()
-            require('nvim-treesitter').setup {}
-
-            require('nvim-treesitter').install {
-                'c', 'cpp', 'diff', 'json', 'lua', 'markdown',
-                'markdown_inline', 'proto', 'python', 'query',
-                'vim', 'vimdoc',
+            require('nvim-treesitter').setup {
+                ensure_installed = {
+                    'c', 'cpp', 'diff', 'json', 'lua', 'markdown',
+                    'markdown_inline', 'proto', 'python', 'query',
+                    'vim', 'vimdoc',
+                }
             }
 
             vim.api.nvim_create_autocmd('FileType', {
