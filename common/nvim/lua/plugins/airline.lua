1e3043a497dc18349fe07a9710da542a6a7ad53c
j <jpnski@protonmail.ch>
2026-04-18 22:46:57 -0700

    nvim theme, tmux theme

--- common/nvim/lua/plugins/airline.lua
+++ common/nvim/lua/plugins/airline.lua
@@ -2,7 +2,7 @@ return {
     {
         'vim-airline/vim-airline',
         config = function()
-            vim.g.airline_theme = "bubblegum"
+            vim.g.airline_theme = "nord_minimal"
             vim.g.airline_powerline_fonts = 0
             vim.g.airline_extensions_whitespace_enabled = 0
         end
